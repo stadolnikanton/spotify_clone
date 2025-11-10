80f69138f29769d130e0c7d45adf9ae77ee289ce
Anton Stadolnik <stadonikanton1@gmail.com>
2025-11-10 20:22:38 +0300

    Изменение функционального представления на классовое

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,11 @@ import debug_toolbar
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from app.views import register_view, main_view, ArtisView
+from app.views import Register, Main, ArtisView
 
 urlpatterns = [
-    path('', main_view, name='main'),
-    path('register/', register_view, name='register'),
+    path('', Main.as_view(), name='main'),
+    path('register/', Register.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('artist/<int:artist_id>/', ArtisView.as_view(), name="artist"),
--- app/views.py
+++ app/views.py
@@ -7,38 +7,45 @@ from app.forms import RegisterForm
 from app.models import Genre, Song, Artist
 
 
-def register_view(request):
-    if request.method == 'POST':
+class Register(View):
+    def get(self, request):
+        form = RegisterForm()
+        return render(request, 'register.html', {'form': form})
+
+    def post(self, request):
         form = RegisterForm(request.POST)
+
         if form.is_valid():
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])  # хэшируем пароль
             user.save()
+
             login(request, user)  # сразу авторизуем пользователя
+
             return redirect('main')  # редирект на главную
-    else:
-        form = RegisterForm()
-    return render(request, 'register.html', {'form': form})
 
 
-def main_view(request):
-    genres = Genre.objects.all()
-    genre_id = request.GET.get('genre')
-    query = request.GET.get('q')
-    songs = Song.objects.all()
-    if genre_id:
-        songs = songs.filter(genre_id=genre_id)
-    if query:
-        songs = songs.filter(
-            Q(title__icontains=query) |
-            Q(artist__name__icontains=query)
-        )
+class Main(View):
+    def get(self, request):
+        genres = Genre.objects.all()
+        genre_id = request.GET.get('genre')
 
-    return render(request, 'main.html', {
-        'songs': songs,
-        'genres': genres,
-        'selected_genre': genre_id
-    })
+        query = request.GET.get('q')
+        songs = Song.objects.all()
+
+        if genre_id:
+            songs = songs.filter(genre_id=genre_id)
+        if query:
+            songs = songs.filter(
+                Q(title__icontains=query) |
+                Q(artist__name__icontains=query)
+            )
+
+        return render(request, 'main.html', {
+            'songs': songs,
+            'genres': genres,
+            'selected_genre': genre_id
+        })
 
 
 class ArtisView(View):
