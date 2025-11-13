b214d423db353631975b207f40b5d36f66f2a627
Anton Stadolnik <stadonikanton1@gmail.com>
2025-11-13 20:30:11 +0300

    Добавлен собственный слой middleware

---
+++ musicPortal/middleware/request_timer.py
@@ -0,0 +1,11 @@
+import time
+
+
+class RequestTimerMiddleWare:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        print("Статус: ")
+        return response
--- musicPortal/settings.py
+++ musicPortal/settings.py
@@ -89,6 +89,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'musicPortal.middleware.request_timer.RequestTimerMiddleWare',
 
 ]
 
